a255f9307225f4f1daaf585dfcd59a284eda3c30
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-06-26 13:09:54 +0530

    ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    // RouterOutlet, 
+    FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Angular19';
+export class AppComponent implements OnInit {
+  title = 'Angular';
+
+  isDisabled: boolean = false
+  count = 0
+
+  imgPath: string = "assets/Angular.jpg"
+
+  ngOnInit(): void {
+
+
+    // setTimeout(() => {
+    //   this.title = this.title + this.count
+    // }, 2000);
+
+    // setTimeout(() => {
+    //   this.title = this.title + 1
+    // }, 4000);
+
+  }
+
+  updateValue(a: any) {
+    console.log('a = ', a);
+    this.count++
+    this.title = this.title + 1
+  }
+
+  keypressed(event: any) {
+    console.log('event.shiftKey = ', event.shiftKey);
+    console.log('event.key = ', event.key);
+  }
+
+
 }
