f73e34ae4e421127bf6a4432333ea46b45c4a6c1
Arvind Kumar Chaurasia <develop.akumar@gmail.com>
2025-06-29 20:46:23 +0530

    HostListener example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component, ElementRef, HostBinding, HostListener, OnInit, viewChild, ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { ChildCompComponent } from './child-comp/child-comp.component';
@@ -11,75 +14,27 @@ import { HighlightElementDirective } from './highlight-element.directive';
     // RouterOutlet, 
     CommonModule,
     FormsModule,
-    ChildCompComponent,
-    HighlightElementDirective
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'Angular';
+export class AppComponent implements AfterViewInit {
 
-  isDisabled: any
-  count = 0
+  @ViewChild('color') colorInput!: ElementRef
 
-  imgPath: string = "assets/Angular.jpg"
+  // @HostBinding('style.backgroundColor') 
+  selectedColor: string = '#000000'
 
-  myObject = { id: 1, name: "apple" }
-
-  array = [
-    { id: 1, name: "apple" },
-    { id: 2, name: "mango" },
-    { id: 3, name: "banana" },
-    { id: 4, name: "watermelon" }
-  ]
-  mathValue: number = 0
-  ngOnInit(): void {
-
-
-    // setTimeout(() => {
-    //   this.title = this.title + this.count
-    // }, 2000);
-
-    // setTimeout(() => {
-    //   this.title = this.title + 1
-    // }, 4000);
-    
+  @HostListener('input', ['$event.target.value'])
+  onColorChange(color: string) {
+    console.log('color = ', color);
+    this.selectedColor = color
   }
 
-  updateValue(a: any) {
-    console.log('a = ', a);
-    this.count++
-    this.title = this.title + 1
-  }
 
-  keypressed(event: any) {
-    console.log('event.shiftKey = ', event.shiftKey);
-    console.log('event.key = ', event.key);
-  }
-
-  trackByFunction(item: any) {
-    return item.id
-  }
-
-  updateItem() {
-    this.array[1] = { id: 2, name: "grapes" }
-
-    setInterval(() => {
-      this.mathValue = Math.floor(Math.random() * 10)
-    }, 1000);
-
-    console.log('this.wrapper = ', this.wrapper);
-     const divElement:HTMLElement = this.wrapper.nativeElement;
-    divElement.style.color = 'maroon'
-  }
-
-  @ViewChild('wrapper') wrapper!:ElementRef
-
-
-  add() {
-   
-
+  ngAfterViewInit() {
+    this.selectedColor = this.colorInput.nativeElement.value
   }
 
 }
