c671046a1cea1140ae7155f552ba694844666eb7
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-06-30 12:00:00 +0530

    custom Number sort pipe implemented

---
+++ src/app/pipes/sort-number.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sortNumber'
+})
+export class SortNumberPipe implements PipeTransform {
+
+  transform(value: number[], order: string): number[] {
+    if (order === 'asc') {
+      return value.sort((a, b) => a - b)
+    }
+    else if (order === 'desc') {
+      return value.sort((a, b) => b - a)
+    }
+    return value;
+  }
+
+}
