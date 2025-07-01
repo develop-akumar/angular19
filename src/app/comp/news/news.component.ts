9c36860d0ca4e32e2b29cb1374cf0d4719a793a5
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-07-01 17:48:10 +0530

    Routing

--- src/app/comp/news/news.component.ts
+++ src/app/comp/news/news.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
 })
 export class NewsComponent {
+
+  constructor(private router: Router) { }
+
+  navigate1() {
+    this.router.navigate(["news/newschild1"], { queryParams: { id: 12, name: "Rohan" } })
+  }
+
+  navigate2() {
+    this.router.navigate(["news/newschild2"])
+    // { queryParams: { id: 50, name: "Kajal" } }
+  }
+
+  navigateByUrl() {
+    this.router.navigateByUrl("/news/newschild1")
+  }
 
 }
