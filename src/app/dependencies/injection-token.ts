1073f1b8e7e81a7b2ff69ec0bc1bd966d9e8a482
arvind.chaurasia <arvind.chaurasia@NDLLAPARVINDKC>
2025-07-02 16:03:45 +0530

    Injection Token

---
+++ src/app/dependencies/injection-token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from "@angular/core";
+
+import { LogMessage1Service } from "../tokenInj/services/log-message1.service";
+
+export const Log_Msg1 = new InjectionToken<LogMessage1Service>("")
