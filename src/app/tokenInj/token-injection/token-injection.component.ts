d25b41ee4178302f55f2f7c423c5d31e464af307
develop-akumar <develop.akumar@gmail.com>
2025-07-03 10:15:34 +0530

    Injection Token

--- src/app/tokenInj/token-injection/token-injection.component.ts
+++ src/app/tokenInj/token-injection/token-injection.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, Inject, inject, OnInit } from '@angular/core';
 import { LogMessage1Service } from '../services/log-message1.service';
 import { LogMessage2Service } from '../services/log-message2.service';
+import { Log_Msg1 } from '../../dependencies/injection-token';
 
 @Component({
   selector: 'app-token-injection',
@@ -9,13 +10,15 @@ import { LogMessage2Service } from '../services/log-message2.service';
   styleUrl: './token-injection.component.scss',
   providers: [
     // LogMessage2Service,
-    // { provide: LogMessage1Service, useClass: LogMessage1Service },
-    { provide: LogMessage2Service, useClass: LogMessage2Service }
+    { provide: Log_Msg1, useClass: LogMessage1Service },
+    // { provide: LogMessage2Service, useClass: LogMessage2Service }
   ]
 })
 export class TokenInjectionComponent implements OnInit {
 
-  public logger = inject(LogMessage2Service)
+  // public logger = inject(LogMessage2Service)
+
+  constructor(@Inject(Log_Msg1) private logger : LogMessage1Service){}
 
   ngOnInit(): void {
     this.logger.log()
