94e1f39ade6896ade454c30501a56e08cabcdf31
Arvind Kumar <develop.akumar@gmail.com>
2025-07-11 12:24:16 +0530

    To do list

--- src/app/comp/Signals/todo-list/todo-list.component.ts
+++ src/app/comp/Signals/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, signal, WritableSignal, AfterViewInit, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,28 +9,75 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrl: './todo-list.component.scss',
+  // standalone: true
 })
-export class TodoListComponent {
+export class TodoListComponent implements AfterViewInit, OnInit, OnDestroy {
 
-  todoList: WritableSignal<string[]> = signal([])
+  constructor() {
+    console.log('console constructor = ',);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  todoList: WritableSignal<string[]> = signal(["Ram"])
 
   textValue: string = '';
 
-  add() {
+  add(text: HTMLInputElement): void {
     this.todoList.update((val: string[]) => {
+      // return [...val, this.textValue]
+      return [...val, text.value]
+    })
+
+    this.textValue = "";
+    text.focus()
+  }
+
+  delete(index: number): void {
+    this.todoList.update((val) =>
+      // val.filter((str) => {
+      //   return str !== val[index]
+      // })
+
+      [...this.todoList().slice(0, index), ...this.todoList().slice(index + 1)]
+    )
+  }
+
+
+  ngAfterViewInit(): void {
+    let button = document.getElementById("btn")
+
+    if (button) {
+      document.addEventListener("keydown", (event) => {
+        console.log('shortcut = ', );
+        if (event.altKey && event.key === "a") {
+          this.add2()
+        }
+      })
+    }
+  }
+
+  add2(): void {
+    this.todoList.update((val: string[]) => {
+      // return [...val, this.textValue]
       return [...val, this.textValue]
     })
 
     this.textValue = "";
+    document.getElementById('addtext')?.focus()
+
   }
 
-  delete(index: number): void {
-    this.todoList.update((val) => {
-      return val.filter((str) => {
-        return str !== val[index]
-      })
-    })
+  ngOnDestroy(): void {
+    document.removeEventListener("keydown", (event) => {
+      console.log('shortcut = ', );
+      if (event.altKey && event.key === "a") {
+        this.add2()
+      }
+    }); 
   }
 
 
