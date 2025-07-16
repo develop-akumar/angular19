97debeb0ff91a2f03a588d7759213b14d48696fa
Arvind Kumar <develop.akumar@gmail.com>
2025-07-16 18:50:52 +0530

    Form array

--- src/app/comp/TemplateDrivenForms/reactive-form/reactive-form.component.ts
+++ src/app/comp/TemplateDrivenForms/reactive-form/reactive-form.component.ts
@@ -9,28 +9,64 @@ import { FormControl, ReactiveFormsModule, Validators, FormGroup, FormArray } fr
 })
 export class ReactiveFormComponent {
 
-  username = new FormControl('',
-    [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(10),
-      Validators.pattern('^[a-zA-Z0-9]+$')
-    ])
 
-    showStatus(){
-      console.log(this.username.errors );
-    }
+
+  showStatus() {
+    console.log(this.username.errors);
+  }
+
+
+
+
+  username!: FormControl
+  groupForm2!: FormGroup;
+  groupForm!: FormGroup
+
+  constructor() {
+
+    this.username = new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        Validators.pattern('^[a-zA-Z0-9]+$')
+      ])
 
     // From Group
-    groupForm:FormGroup = new FormGroup({
+    this.groupForm = new FormGroup({
       username: new FormControl("", [Validators.required]),
-    })  
-
-
-    groupForm2:FormGroup = new FormGroup({
-      username : new FormControl("", [Validators.required]),
-      password : new FormGroup("", [Validators.required]),
-
-      array : new FormArray([])
     })
+
+    // Form array -------------------------------------------------
+    this.groupForm2 = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      password: new FormGroup("", [Validators.required]),
+
+      array: new FormArray([this.getChildForm()])
+    })
+  }
+
+
+  // getter method to access and run for loop in html
+  // also to pass in <div formArrayName="array">
+  get array(): FormArray {
+    return this.groupForm2.get('array') as FormArray;
+  }
+
+  getChildForm(): FormGroup {
+    return new FormGroup({
+      subject: new FormControl(""),
+      marks: new FormControl("")
+    })
+  }
+
+  add() {
+    this.array.push(this.getChildForm())
+  }
+
+  remove(index: any) {
+    this.array.removeAt(index)
+  }
+
+
 }
