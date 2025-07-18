d1fb9c9a56ff980b88d1c9f0dce69dc10d3565d0
Arvind Kumar Chaurasia <develop.akumar@gmail.com>
2025-07-18 12:27:28 +0530

    test component

---
+++ src/app/comp/Signals/task2/task2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task2',
+  imports: [],
+  templateUrl: './task2.component.html',
+  styleUrl: './task2.component.scss'
+})
+export class Task2Component {
+
+  
+
+}
