836108debd526e1e1b8c2455a873022659d89c89
Arvind Kumar Chaurasia <develop.akumar@gmail.com>
2025-07-19 17:28:11 +0530

    Preload all modules

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { NoPreloading, PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,7 +8,10 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withPreloading(PreloadAllModules)  ),
+    provideRouter(routes, withPreloading(
+      // PreloadAllModules,
+      NoPreloading
+    )  ),
     // This is used to make HTTP requests
     // It is necessary for making API calls in the application
     provideHttpClient(),
